25b246e02de320e9421274ecc1d6d953de605b55
Brian <briannaz21@hotmail.com>
2025-04-04 19:02:33 -0500

    added Farenheit

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,23 +1,29 @@
 import React from "react";
 
-const getIconUrl = (day) => {
-  const iconCode = day.weather.icon;
-  return `https://www.weatherbit.io/static/img/icons/${iconCode}.png`;
+const getIconUrl = (condition) => {
+  return `https:${condition.icon}`;
 };
 
-function Forecast({ data }) {
+function Forecast({ forecast, unit }) {
   return (
     <div>
       <div className="flex flex-row items-center justify-between text-white">
-        {data.slice(1, 6).map((day) => (
-          <div key={day.datetime}>
+        {forecast.map((day, idx) => (
+          <div key={idx} className="flex flex-col items-center">
             <p className="font-light text-sm">
-              {new Date(day.valid_date).toLocaleDateString("en-US", {
+              {new Date(day.date).toLocaleDateString("en-US", {
                 weekday: "short",
               })}
             </p>
-            <img src={getIconUrl(data[0])} alt="" className="w-10" />
-            <p className="font-medium">{day.temp.toFixed()}&deg;C</p>
+            <img
+              src={`https:${day.day.condition.icon}`}
+              alt={day.day.condition.text}
+              className="w-10"
+            />
+            <p className="font-medium">{unit === "C"
+                ? day.day.avgtemp_c.toFixed()
+                : day.day.avgtemp_f.toFixed()}
+              °{unit}</p>
           </div>
         ))}
       </div>
